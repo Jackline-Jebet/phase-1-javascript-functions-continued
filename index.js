01e2918924013f22fa67da3cd1f184a73b63a5bf
Jackline Jebet <jebetjackline2016@gmail.com>
2022-06-07 21:53:40 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun (activity='roller-skate'){
+    return`This Saturday, I want to ${activity}!`;
+}
+
+let mondayWork = function(activity='go to the office') {
+    return `This Monday, I will ${activity}.`
+  }
+
+  function wrapAdjective(result="*"){
+    return function(rule="a dedicated programmer"){
+        return `You are ${result}${rule}${result}!`
+    }
+} 
